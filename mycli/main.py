59687a3069a0f93f8a5f8e11115cfd1eb8791fef
zouyang <zouyang@MacBook-Pro.local>
2021-08-28 12:34:23 +0800

    keybindings: enable up/down arrow for history search

--- mycli/main.py
+++ mycli/main.py
@@ -908,6 +908,7 @@ class MyCli:
                 enable_suspend=True,
                 editing_mode=editing_mode,
                 search_ignore_case=True,
+                enable_history_search=True,
             )
 
         try:
