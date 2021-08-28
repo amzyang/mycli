c6802c3ae8d11d5d464d5c01e41d9d05dc053062
amzyang <frederick.zou@gmail.com>
2021-08-28 11:04:10 +0800

    hack: add c-n, c-p keybindings, improve completion

--- mycli/key_bindings.py
+++ mycli/key_bindings.py
@@ -3,6 +3,7 @@ import logging
 from prompt_toolkit.enums import EditingMode
 from prompt_toolkit.filters import completion_is_selected, emacs_mode
 from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.filters.cli import ViInsertMode
 
 from mycli import shortcuts
 from mycli.packages.toolkit.fzf import search_history
@@ -13,6 +14,7 @@ _logger = logging.getLogger(__name__)
 def mycli_bindings(mycli):
     """Custom key bindings for mycli."""
     kb = KeyBindings()
+    insert_mode = ViInsertMode()
 
     @kb.add("f2")
     def _(event):
@@ -169,5 +171,23 @@ def mycli_bindings(mycli):
             event.app.current_buffer.validate_and_handle()
         else:
             event.app.current_buffer.insert_text("\n")
+
+    @kb.add('c-p', filter=insert_mode)
+    def _(event):
+        b = event.current_buffer
+
+        if b.complete_state:
+            b.complete_previous()
+        else:
+            event.current_buffer.auto_up(count=event.arg)
+
+    @kb.add('c-n', filter=insert_mode)
+    def _(event):
+        b = event.current_buffer
+
+        if b.complete_state:
+            b.complete_next()
+        else:
+            event.current_buffer.auto_down(count=event.arg)
 
     return kb
