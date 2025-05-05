f37a3fa0c577f7399d8213901f079025236ffc2a
amzyang <amzyang@example.com>
2025-05-05 22:15:54 +0800

    feat(mycli): add cursor shape configuration for improved visual feedback

--- mycli/main.py
+++ mycli/main.py
@@ -23,6 +23,7 @@ from cli_helpers.utils import strip_ansi
 import click
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.completion import DynamicCompleter
+from prompt_toolkit.cursor_shapes import ModalCursorShapeConfig
 from prompt_toolkit.document import Document
 from prompt_toolkit.enums import DEFAULT_BUFFER, EditingMode
 from prompt_toolkit.filters import HasFocus, IsDone
@@ -906,6 +907,7 @@ class MyCli(object):
                 editing_mode=editing_mode,
                 search_ignore_case=True,
                 enable_history_search=True,
+                cursor=ModalCursorShapeConfig(),
             )
 
         try:
